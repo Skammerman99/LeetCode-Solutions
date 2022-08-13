8518f58cff0d643c79fae14bf1270a2664f261cd
Spencer Kammerman <spencerkam99@gmail.com>
2022-08-13 16:12:07 -0700

    Time: 20 ms (6.26%), Space: 7 MB (6.30%) - LeetHub

---
+++ 202-happy-number/202-happy-number.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        vector<int> reached;
+        return isHappyRecursive(n, reached);
+        
+    }
+    
+    bool isHappyRecursive(int n, vector<int>& reached){
+        std::vector< int > arr;
+        while (n >= 10){
+            arr.push_back(n%10);
+            n = n/10;
+        }
+        arr.push_back(n);
+        int sum = 0;
+        for(int num:arr){
+            std::cout << num << std::endl;
+            sum += pow(num,2);
+        }
+        if(sum == 1){
+            return true;
+        }
+        else if(std::find(reached.begin(), reached.end(), sum) != reached.end()){
+            return false;
+        }
+        else{
+            reached.push_back(sum);
+            return isHappyRecursive(sum,reached);
+        }
+    
+    }
+};
