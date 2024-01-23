a67cafa37419d86a58ec9770c87e6cee34c5930b
Spencer Kammerman <spencerkam99@gmail.com>
2024-01-23 15:50:22 -0600

    Time: 0 ms (100.00%), Space: 7.8 MB (10.77%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     std::map<int,int> solved;
     int climbStairs(int n) {
-        if(n == 1){
+        if(solved.find(n) != solved.end()){
+            return solved[n];
+        }else if(n == 1){
             return 1;
         }else if (n == 2){
             return 2;
-        }else if(solved.find(n) != solved.end()){
-            return solved[n];
-        }
-        else{
+        }else{
             solved[n] = climbStairs(n - 1) + climbStairs(n - 2);
             return solved[n];
         }
