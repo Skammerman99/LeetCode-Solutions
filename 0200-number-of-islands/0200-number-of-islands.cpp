91a0b25fa2db8a7754f253d07bfe27e2c39b4223
Spencer Kammerman <spencerkam99@gmail.com>
2024-01-23 00:02:28 -0600

    Time: 128 ms (5.52%), Space: 41.8 MB (5.01%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    std::set<std::pair<int,int>> checked;
+    int numIslands(vector<vector<char>>& grid) {        
+        int sum = 0;
+        for(int i = 0; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if(!checked.contains(std::make_pair(i,j)) && isIsland(grid,i,j)){
+                    sum ++;
+                }
+            }
+        }
+        return sum;
+        
+    }
+    
+    bool isIsland(vector<vector<char>>& grid, int i, int j){
+        //std::cout << "i|j: " << i << " | " << j << std::endl; 
+        if(grid[i][j] == '0' ){
+            checked.insert(std::make_pair(i,j));
+            //std::cout << "found water" << std::endl;
+            return false;
+        }
+        else{
+            //std::cout << "found land" << std::endl;
+            checked.insert(std::make_pair(i,j));            
+            if(i > 0 && !checked.contains(std::make_pair(i-1,j))){
+                isIsland(grid,i-1,j);
+                checked.insert(std::make_pair(i-1,j));            
+            }
+            if(i < grid.size() - 1 && !checked.contains(std::make_pair(i+1,j))){
+                isIsland(grid,i+1,j);
+                checked.insert(std::make_pair(i+1,j));
+                }
+            if(j > 0 && !checked.contains(std::make_pair(i,j-1))){
+                isIsland(grid,i,j - 1);
+                checked.insert(std::make_pair(i,j-1));
+            }
+            if(j < grid[0].size() - 1 && !checked.contains(std::make_pair(i,j+1))){
+                isIsland(grid,i,j+1);
+                checked.insert(std::make_pair(i,j+1));
+            }
+            return true;
+        }
+    }
+};
