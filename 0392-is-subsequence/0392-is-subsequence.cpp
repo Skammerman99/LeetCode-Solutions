f17da8da406ba4d5a11fea0e8f18d43172982c72
Spencer Kammerman <spencerkam99@gmail.com>
2024-02-08 19:13:53 -0800

    Time: 0 ms (100.00%), Space: 7.4 MB (35.28%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(t.length() == 0){
+            return s.length() == 0;
+        }
+        int i = 0, j = 0;
+        while(j < t.length()){
+            if (s[i] == t[j]){
+                i++;
+            }
+            j++;
+            if(i == s.length()){
+                return true;
+            }
+        }
+        return false;
+    }
+};
