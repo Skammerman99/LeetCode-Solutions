fb2a17d03b0f4f35ca6b238c238e41789976b1a6
Spencer Kammerman <spencerkam99@gmail.com>
2024-03-12 17:20:16 -0700

    Time: 15 ms (69.72%), Space: 17.7 MB (16.04%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(!lists.size()){
+            return nullptr;
+        }
+        vector<int> compiledList;
+        for(auto list : lists){
+            if(list != nullptr){
+                compiledList.push_back(list->val);
+                while(list->next != nullptr){
+                    list = list->next;
+                    compiledList.push_back(list->val);
+                }
+            }
+        }
+        if(!compiledList.size()){
+            return nullptr;
+        }
+        std::sort(compiledList.begin(), compiledList.end());
+        ListNode* resultHead = new ListNode;
+        ListNode* resultIndex = resultHead;
+        int count = 0;
+        for(auto num : compiledList){
+            resultIndex->val = num;
+            count++;
+            if(count != compiledList.size()){
+                resultIndex->next = new ListNode;
+                resultIndex = resultIndex->next;
+            }
+        }
+        resultIndex = nullptr;
+        return resultHead;
+    }
+};
