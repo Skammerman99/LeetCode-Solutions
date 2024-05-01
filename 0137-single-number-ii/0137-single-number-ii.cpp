a950e990b6658855f78c273bf310936529249f06
Spencer Kammerman <spencerkam99@gmail.com>
2024-04-30 21:43:21 -0700

    Time: 8 ms (35.90%), Space: 11.9 MB (36.93%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        std::sort(nums.begin(),nums.end());
+        int last = nums[0];
+        int count = 1;
+        for(int i = 1; i < nums.size(); i++){
+            std::cout << "last: " << last << std::endl;
+            std::cout << "count: " << count << std::endl;
+            if(nums[i] == last){
+                count++;
+            }else if(count == 3){
+                last = nums[i];
+                count = 1;
+            }else if(count == 1){
+                return last;
+            }
+        }
+        return last;
+    }
+};
